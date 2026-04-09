1dc5edf9603023930d848aaf9b74c409383b2b2c
Taysha C <legacycode2024@gmail.com>
2026-04-09 12:44:18 -0400

    Add discount popup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from 'next';
 import { CartProvider } from './context/CartContext';
 import Footer from './components/Footer';
+import DiscountPopup from './components/DiscountPopup';
 
 export const viewport = {
   width: 'device-width',
@@ -35,6 +36,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <CartProvider>
           {children}
           <Footer />
+          <DiscountPopup />
         </CartProvider>
         <Analytics />
       </body>
