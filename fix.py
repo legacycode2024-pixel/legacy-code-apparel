5d9d22f0d841a91366875855d72984fa171901ed
Taysha C <legacycode2024@gmail.com>
2026-04-25 00:55:13 -0400

    remove right over easy

--- fix.py
+++ fix.py
@@ -1,13 +1,5 @@
 content = open('src/app/page.tsx').read()
-old = '''      <video ref={vid1Ref} muted playsInline onEnded={handleVid1End} style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', zIndex: 0, opacity: 1, transition: 'opacity 1.5s' }}>
-        <source src="/images/standonit-video.mov" type="video/mp4" />
-      </video>
-      <video ref={vid2Ref} muted playsInline onEnded={handleVid2End} style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', zIndex: 0, opacity: 0, transition: 'opacity 1.5s' }}>
-        <source src="/images/variety-tees.mov" type="video/mp4" />
-      </video>'''
-new = '''      <video ref={vid1Ref} muted playsInline loop style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', zIndex: 0, opacity: 1, transition: 'opacity 1.5s' }}>
-        <source src="/images/variety-tees.mov" type="video/mp4" />
-      </video>'''
-content = content.replace(old, new)
+content = content.replace("  { id: 4, name: 'Right Over Easy', color: 'Black', price: 20, frontImage: 'https://res.cloudinary.com/dozyoetnr/image/upload/v1776660208/right-front-black_kz6ufo.jpg', backImage: 'https://res.cloudinary.com/dozyoetnr/image/upload/v1776660208/right-back-black_jiw0ef.jpg' },\n", "")
+content = content.replace("  { id: 10, name: 'Right Over Easy', color: 'White', price: 20, frontImage: 'https://res.cloudinary.com/dozyoetnr/image/upload/v1776660208/right-front-white_lwgedo.jpg', backImage: 'https://res.cloudinary.com/dozyoetnr/image/upload/v1776660208/right-back-white_ns3dew.jpg' },\n", "")
 open('src/app/page.tsx', 'w').write(content)
 print('Done!')
