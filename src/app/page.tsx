c3238b0de9cdff34845e953318bd4b6e74b7c2c0
Taysha C <legacycode2024@gmail.com>
2026-09-25 12:43:43 -0400

    Update black back model image for Nothing to Hide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -81,7 +81,7 @@ const preorderProducts: PreorderProduct[] = [
     colorImages: {
       Black: {
         front: 'https://res.cloudinary.com/dozyoetnr/image/upload/v1789964263/C98A6C3B-2E76-46C4-8365-42C618727593_nun4on.jpg',
-        back: 'https://res.cloudinary.com/dozyoetnr/image/upload/v1788638035/word-is-bond-crop-product_wwuorv.png',
+        back: 'https://res.cloudinary.com/dozyoetnr/image/upload/f_auto/q_auto/C98A6C3B-2E76-46C4-8365-42C618727593_crw2wc.jpg',
         model: 'https://res.cloudinary.com/dozyoetnr/image/upload/ar_1:1,b_gen_fill,c_pad/e_enhance/r_20/f_auto/q_auto/67E05BD3-1C45-4F50-84FC-46A074A60E89_qohshq.jpg',
       },
       Blue: {
